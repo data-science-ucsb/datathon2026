0e7528b92cd03bbc6d73787bdef8bed723534686
Kishan Goli <kishangoli@ucsb.edu>
2026-02-17 14:48:20 -0800

    Update OrganizerTeamsSandbox.tsx

--- src/components/dashboard/OrganizerTeamsSandbox.tsx
+++ src/components/dashboard/OrganizerTeamsSandbox.tsx
@@ -273,7 +273,6 @@ const OrganizerTeamsSandbox: React.FC<OrganizerTeamsSandboxProps> = ({
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                   </svg>
                   <p>No teams created yet</p>
-                  <p className="text-sm mt-1">Create your first team above to get started</p>
                 </div>
               )}
             </div>
